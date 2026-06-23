8655fa67b1a6f9c06afc65ff1ceebd2853de5176
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2026-06-23 20:44:46 +0600

    add - create new job application

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,58 @@
-export default function Dashboard () {
-    return <div>Dashboard page</div>
+import KanbanBoard from "@/components/kanban-board";
+import { getSession } from "@/lib/auth/auth";
+import connectionDB from "@/lib/db";
+import { Board } from "@/lib/models";
+import { redirect } from "next/navigation";
+import { Suspense } from "react";
+
+async function getBoard(userId: string) {
+    "use cache";
+    await connectionDB();
+
+    const boardDoc = await Board.findOne({
+        userId: userId,
+        name: "Job Hunt"
+    }).populate({
+        path: "columns",
+        populate: {
+            path: "jobApplications",
+        }
+    });
+
+    if(!boardDoc) return null;
+
+    // const board = boardDoc ? boardDoc.toObject() : null;
+    const board = JSON.parse(JSON.stringify(boardDoc));
+
+    return board;
+} 
+
+async function DashboardPage() {
+    const session = await getSession();
+    const board = await getBoard(session?.user.id ?? "");
+
+    if(!session?.user){
+        redirect("/sign-in");
+    }
+
+    return (
+        <div className="min-h-screen bg-white">
+            <div className="container mx-auto p-6">
+                <div className="mb-6">
+                    <h1 className="text-3xl font-bold text-black">{board.name}</h1>
+                    <p className="text-gray-600">Track your job application</p>
+                </div>
+                <KanbanBoard board={board} userId={session.user.id} />
+            </div>
+        </div>
+    )
+}
+
+
+export default async function Dashboard () {
+    return (
+        <Suspense fallback={<p>Loading...</p>}>
+            <DashboardPage/>
+        </Suspense>
+    )
 }
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -3,6 +3,7 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { initializeUserBoard } from "../init-user-board";
 
 const client = new MongoClient(process.env.MONGODB_URI!);
 const db = client.db();
@@ -11,14 +12,31 @@ export const auth = betterAuth({
     database: mongodbAdapter(db, {
         client,
     }),
+    session: {
+        cookieCache: {
+            enabled: true,
+            maxAge: 60 * 60,
+        },
+    },
     emailAndPassword: {
         enabled: true
     },
+    databaseHooks: {
+        user: {
+            create: {
+                after: async (user) => {
+                    if(user.id) {
+                        await initializeUserBoard(user.id);
+                    }
+                }
+            }
+        }
+    }
 });
 
 export async function getSession() {
     const result = await auth.api.getSession({
-        headers: await headers()
+        headers: await headers(),
     });
     return result;
 }
